c5a9f6608fe2cce0c64b9c37214506c1fbe6c6b8
Gurjit <gurjiitsingh@gmail.com>
2026-06-12 16:50:50 +0530

    update

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
 
                 <FooterWrapper />
 
-                <CartBottomWrapper />
+                {/* <CartBottomWrapper /> */}
               </div>
             </Providers>
 
